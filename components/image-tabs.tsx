b70cad82f6b3d70c7bee6da43a5ad4c88f334606
SabaYosef <sabayosef180@gmail.com>
2026-04-11 21:36:25 +0300

    chore: initialize ImageTabs component with section layout

---
+++ components/image-tabs.tsx
@@ -0,0 +1,11 @@
+export default function ImageTabs() {
+  return (
+    <section className="border-t bg-white py-16">
+      <div className="container mx-auto px-4">
+        <div className="mx-auto max-w-6xl">
+          {/* Content will go here */}
+        </div>
+      </div>
+    </section>
+  );
+}
