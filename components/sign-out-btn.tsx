bb02b4d12716ecde2a8b519b3e2769cc508214b2
SabaYosef <sabayosef180@gmail.com>
2026-04-14 19:51:06 +0300

    feat: add useRouter hook for post-signout navigation

--- components/sign-out-btn.tsx
+++ components/sign-out-btn.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { DropdownMenuItem } from "./ui/dropdown-menu";
+import { useRouter } from "next/navigation";
 
 export default function SignOutButton() {
+    const router = useRouter();
   return (
     <DropdownMenuItem>
       Log Out
