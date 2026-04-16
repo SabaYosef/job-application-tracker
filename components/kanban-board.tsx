43f0b0aef5313f3bcada22d3aad4650f1827cb80
SabaYosef <sabayosef180@gmail.com>
2026-04-16 21:21:37 +0300

    chore: initialize kanban board structure and column configuration

--- components/kanban-board.tsx
+++ components/kanban-board.tsx
@@ -1,9 +1,74 @@
-"use server";
+"use client";
 
-import connectDB from "@/lib/db";
-import { JobApplication, Column } from "@/lib/models";
-import { revalidatePath } from "next/cache";
+import { Board, Column, JobApplication } from "@/lib/models/models.types";
+import {
+  Award,
+  Calendar,
+  CheckCircle2,
+  Mic,
+  MoreHorizontal,
+  MoreVertical,
+  Trash2,
+  XCircle,
+} from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import CreateJobApplicationDialog from "./create-job-dialog";
+import JobApplicationCard from "./job-application-card";
+import { useBoard } from "@/lib/hooks/useBoards";
+import {
+  closestCorners,
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+  PointerSensor,
+  useDroppable,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { useState } from "react";
 
-export async function createJobApplication(data: any) {
-  
+interface KanbanBoardProps {
+  board: Board;
+  userId: string;
 }
+
+interface ColConfig {
+  color: string;
+  icon: React.ReactNode;
+}
+const COLUMN_CONFIG: Array<ColConfig> = [
+  {
+    color: "bg-cyan-500",
+    icon: <Calendar className="h-4 w-4" />,
+  },
+  {
+    color: "bg-purple-500",
+    icon: <CheckCircle2 className="h-4 w-4" />,
+  },
+  {
+    color: "bg-green-500",
+    icon: <Mic className="h-4 w-4" />,
+  },
+  {
+    color: "bg-yellow-500",
+    icon: <Award className="h-4 w-4" />,
+  },
+  {
+    color: "bg-red-500",
+    icon: <XCircle className="h-4 w-4" />,
+  },
+];
